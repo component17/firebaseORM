35233f5a2375df658e870f2193b95498c762dc3d
k1on <mail@k1on.ru>
2017-08-09 13:05:33 +0300

    First alpha

--- Model.js
+++ Model.js
@@ -1,36 +1,32 @@
-import firebase from 'firebase';
-import Vue from 'vue';
-export class moModeldel {
-    constructor(fields, table) {
-        this.db = window.firebase;
-        this.app = Vue;
+const firebase = require('firebase');
+const firebaseORM = class firebaseorm {
+    constructor() {}
+    __construct() {
+
         if (!firebase.apps.length) {
 
-            firebase.initializeApp({
-
-            });
+            firebase.initializeApp(this.firebase);
         }
 
-        this.fields = fields;
-        this.table = table;
+        if(this.fields) {
 
-        for(let key in this.fields) {
-            if(typeof this.fields[key] === "object") {
-                this[key] = this.fields[key].hasOwnProperty('defaults') ? this.fields[key].defaults : '';
+            for(let key in this.fields) {
+                if(typeof this.fields[key] === "object") {
+                    this[key] = this.fields[key].hasOwnProperty('defaults') ? this.fields[key].defaults : '';
 
-            } else {
-                this[this.fields[key]] = '';
+                } else {
+                    this[this.fields[key]] = '';
+                }
             }
         }
-
     }
 
-     getErrors() {
+    getErrors() {
         return this.errors;
     }
 
 
-     getErrorFields() {
+    getErrorFields() {
         return this.error_fields;
     }
 
@@ -55,8 +51,65 @@ export class moModeldel {
     }
 
 
-    manyToMany(__table, __from, __to) {
+    belongsToMany(__table, __from, __to) {
+        let self = this;
 
+        return {
+            get(callback) {
+                //let key = self.encode().encode(self.key + model.key);
+                self[__to] = [];
+                firebase.database().ref(__table).orderByChild(__from).equalTo(self.key).on('child_added', (snap) => {
+                    let s = snap.val();
+
+                firebase.database().ref(__to+"/"+s[__to]).on('value', (_snap) => {
+                    let result = {};
+                let _s = _snap.val();
+                for(let key in _s) {
+                    result[key] = _s[key];
+                }
+                result['key'] = _snap.key;
+                if(s.pivot) {
+                    result['pivot'] = s.pivot;
+                }
+
+
+                if(self[__to].length) {
+                    for(let key in self[__to]) {
+                        if(self[__to][key].key === _snap.key) {
+                            self[__to][key] = result;
+                        } else {
+                            self[__to].push(result);
+                        }
+                    }
+                } else {
+                    self[__to].push(result);
+                }
+
+                callback(self);
+            })
+            })
+            },
+
+            attach: function(model, pivot = null) {
+                let object = {};
+                object[__from] = self.key;
+                object[__to]   = model.key;
+
+                if(pivot) {
+                    object['pivot'] = pivot;
+                }
+
+                let key = self.encode().encode(self.key + model.key);
+
+                firebase.database().ref(__table + "/" + key).set(object)
+            },
+
+            detach: function(model) {
+                let key = self.encode().encode(self.key + model.key);
+
+                firebase.database().ref(__table + "/" + key).remove();
+            }
+        }
     }
 
 
@@ -69,17 +122,17 @@ export class moModeldel {
 
                 firebase.database().ref(__table).orderByChild("rel_"+__key).equalTo(self.key).on('value', (snap) => {
                     let obj = snap.val();
-                    let result = [];
-                    self[__table] = [];
-                    for(let key in obj) {
-                        let data = Object.assign({}, obj[key], {key: key});
-                        result.push(data);
-                        self[__table].push(data);
+                let result = [];
+                self[__table] = [];
+                for(let key in obj) {
+                    let data = Object.assign({}, obj[key], {key: key});
+                    result.push(data);
+                    self[__table].push(data);
 
-                    }
+                }
 
-                    callback(result);
-                })
+                callback(result);
+            })
             }
         }
     }
@@ -93,13 +146,13 @@ export class moModeldel {
                 let id = self['rel_'+__key];
                 firebase.database().ref(__table+"/"+id).on('value', (snap) => {
                     let object = snap.val();
-                    self[__key] = {};
-                    for(let key in object) {
-                        self[__key][key] = object[key];
-                    }
-                    self[__key]['key'] = snap.key;
-                    callback(self);
-                });
+                self[__key] = {};
+                for(let key in object) {
+                    self[__key][key] = object[key];
+                }
+                self[__key]['key'] = snap.key;
+                callback(self);
+            });
             },
 
             attach: function (model) {
@@ -109,7 +162,7 @@ export class moModeldel {
                 firebase.database().ref(self.table + "/" + self.key).update(update);
             },
 
-            dettach: function () {
+            detach: function () {
                 let update = {};
                 update["rel_" + __key] = null;
                 firebase.database().ref(self.table + "/" + self.key).update(update);
@@ -120,10 +173,15 @@ export class moModeldel {
 
     }
 
-    all(callback) {
-        this.db.child(this.table).on('value', (snap) => {
-            callback(snap.val());
-        });
+    all(callback = null) {
+        return new Promise((resolve, reject) => {
+                firebase.database().ref(this.table).on('value', (snap) => {
+                if(callback) {
+                    callback(snap.val());
+                }
+            });
+    })
+
     }
 
     find(id, callback) {
@@ -131,28 +189,28 @@ export class moModeldel {
 
         console.log(this.table + "/" + id);
         return new Promise((resolve, reject) => {
-            let method = typeof callback === "function" ? 'on' : 'once';
-            firebase.database().ref(this.table + "/" + id)[method]('value', (snap) => {
+                let method = typeof callback === "function" ? 'on' : 'once';
+        firebase.database().ref(this.table + "/" + id)[method]('value', (snap) => {
 
-                let snapshot = snap.val();
+            let snapshot = snap.val();
 
-                if(snapshot) {
-                    let result = {};
-                    for(let key in snapshot) {
-                        self[key] = snapshot[key];
-                        result[key] = snapshot[key];
-                    }
-                    result.key = snap.key;
-                    self.key = snap.key;
-                    if(typeof callback === "function") {
-                        callback(result);
-                    }
-                    resolve(this);
-                } else {
-                    reject("row not found")
-                }
-            })
-        })
+        if(snapshot) {
+            let result = {};
+            for(let key in snapshot) {
+                self[key] = snapshot[key];
+                result[key] = snapshot[key];
+            }
+            result.key = snap.key;
+            self.key = snap.key;
+            if(typeof callback === "function") {
+                callback(result);
+            }
+            resolve(this);
+        } else {
+            reject("row not found")
+        }
+    })
+    })
 
     }
 
@@ -202,8 +260,8 @@ export class moModeldel {
 
             eval(gf).on('child_added', (snap) => {
                 console.log(snap);
-                callback(Object.assign({}, {key: snap.key}, snap.val()));
-            })
+            callback(Object.assign({}, {key: snap.key}, snap.val()));
+        })
         };
 
         o.last = function (callback) {
@@ -215,8 +273,8 @@ export class moModeldel {
 
             eval(gf).on('child_added', (snap) => {
                 console.log(snap);
-                callback(Object.assign({}, {key: snap.key}, snap.val()));
-            })
+            callback(Object.assign({}, {key: snap.key}, snap.val()));
+        })
         };
 
         o.limit = function (limit = 25) {
@@ -232,20 +290,20 @@ export class moModeldel {
 
             return new Promise((resolve, reject) => {
                 eval(gf)[method]('value', (snap) => {
-                    let result = [];
+                let result = [];
 
-                    let obj = snap.val();
-                    for(let key in obj) {
-                        let data = Object.assign({}, obj[key], {key: key});
-                        result.push(data);
-                    }
+                let obj = snap.val();
+                for(let key in obj) {
+                    let data = Object.assign({}, obj[key], {key: key});
+                    result.push(data);
+                }
 
-                    if(typeof callback === 'function')
-                        callback(result);
+                if(typeof callback === 'function')
+                    callback(result);
 
-                    resolve(result);
-                })
-            });
+                resolve(result);
+            })
+        });
 
         };
 
@@ -268,13 +326,13 @@ export class moModeldel {
 
         return new Promise((resolve, reject) => {
 
-            if(this.validParams(params) === false) {
-                reject(this.getErrors());
-            } else {
-                this.key = firebase.database().ref(this.table).push(params).key;
-                resolve(this.key);
-            }
-        });
+                if(this.validParams(params) === false) {
+            reject(this.getErrors());
+        } else {
+            this.key = firebase.database().ref(this.table).push(params).key;
+            resolve(this.key);
+        }
+    });
 
     }
 
@@ -283,9 +341,9 @@ export class moModeldel {
         this.validParams(params);
 
         return new Promise((resolve, reject) => {
-            firebase.database().ref(this.table + "/" + key).update(params);
-            resolve(this);
-        });
+                firebase.database().ref(this.table + "/" + key).update(params);
+        resolve(this);
+    });
 
     }
 
@@ -301,9 +359,9 @@ export class moModeldel {
         }
 
         return new Promise((resolve, reject) => {
-            firebase.database().ref(this.table + "/" + key).remove();
-            resolve();
-        });
+                firebase.database().ref(this.table + "/" + key).remove();
+        resolve();
+    });
     }
 
 
@@ -317,8 +375,6 @@ export class moModeldel {
         }
 
 
-        console.log(this.fields);
-
         let f = this.fields;
 
         let errors = [],
@@ -326,8 +382,7 @@ export class moModeldel {
 
         for(let key in f) {
             let row = f[key];
-            console.log("key", key);
-            console.log("row", row);
+
             if(typeof row === "object") {
 
                 if(row.hasOwnProperty('required')) {
@@ -410,4 +465,106 @@ export class moModeldel {
 
     }
 
+
+    encode(string) {
+        let Base64 = {
+            _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+            encode: function (e) {
+                var t = "";
+                var n, r, i, s, o, u, a;
+                var f = 0;
+                e = Base64._utf8_encode(e);
+                while (f < e.length) {
+                    n = e.charCodeAt(f++);
+                    r = e.charCodeAt(f++);
+                    i = e.charCodeAt(f++);
+                    s = n >> 2;
+                    o = (n & 3) << 4 | r >> 4;
+                    u = (r & 15) << 2 | i >> 6;
+                    a = i & 63;
+                    if (isNaN(r)) {
+                        u = a = 64
+                    } else if (isNaN(i)) {
+                        a = 64
+                    }
+                    t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a)
+                }
+                return t
+            },
+            decode: function (e) {
+                var t = "";
+                var n, r, i;
+                var s, o, u, a;
+                var f = 0;
+                e = e.replace(/[^A-Za-z0-9+/=]/g, "");
+                while (f < e.length) {
+                    s = this._keyStr.indexOf(e.charAt(f++));
+                    o = this._keyStr.indexOf(e.charAt(f++));
+                    u = this._keyStr.indexOf(e.charAt(f++));
+                    a = this._keyStr.indexOf(e.charAt(f++));
+                    n = s << 2 | o >> 4;
+                    r = (o & 15) << 4 | u >> 2;
+                    i = (u & 3) << 6 | a;
+                    t = t + String.fromCharCode(n);
+                    if (u != 64) {
+                        t = t + String.fromCharCode(r)
+                    }
+                    if (a != 64) {
+                        t = t + String.fromCharCode(i)
+                    }
+                }
+                t = Base64._utf8_decode(t);
+                return t
+            },
+            _utf8_encode: function (e) {
+                e = e.replace(/rn/g, "n");
+                var t = "";
+                for (var n = 0; n < e.length; n++) {
+                    var r = e.charCodeAt(n);
+                    if (r < 128) {
+                        t += String.fromCharCode(r)
+                    } else if (r > 127 && r < 2048) {
+                        t += String.fromCharCode(r >> 6 | 192);
+                        t += String.fromCharCode(r & 63 | 128)
+                    } else {
+                        t += String.fromCharCode(r >> 12 | 224);
+                        t += String.fromCharCode(r >> 6 & 63 | 128);
+                        t += String.fromCharCode(r & 63 | 128)
+                    }
+                }
+                return t
+            },
+            _utf8_decode: function (e) {
+                var t = "";
+                var n = 0;
+                var r = c1 = c2 = 0;
+                while (n < e.length) {
+                    r = e.charCodeAt(n);
+                    if (r < 128) {
+                        t += String.fromCharCode(r);
+                        n++
+                    } else if (r > 191 && r < 224) {
+                        c2 = e.charCodeAt(n + 1);
+                        t += String.fromCharCode((r & 31) << 6 | c2 & 63);
+                        n += 2
+                    } else {
+                        c2 = e.charCodeAt(n + 1);
+                        c3 = e.charCodeAt(n + 2);
+                        t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
+                        n += 3
+                    }
+                }
+                return t
+            }
+        };
+
+
+        return Base64;
+
+    }
+
+}
+
+module.exports = function() {
+    return new firebaseORM;
 }
--- README.md
+++ README.md
@@ -1,3 +1,216 @@
-# firebaseORM
-Firebase ORM
+# Reactive Google Firebase ORM
+Reactive Google Firebase ORM
+
+**!!! Alpha Version !!!**
+
 https://github.com/component17/firebaseORM/wiki
+
+# Methods
+* create
+* update
+* delete
+* find
+* save
+* query
+* all
+
+
+| method | params | description |
+| ----- | ----- | ---------- |
+| create |`params*`| Model object with data e.g `{title: "Post title", text: "new post"}`|
+| update | `key*`, `params*` | update row with key `key` |
+| delete | `key*` | remove row. if key not set, row will be removed with model key, if has call  after `Post.find()` e.g, or  manual setted by `Model.key = 'somekey'`|
+| find | `key*`, `callback` | find row with key `key` |
+| save | | save model |
+| query | | see bottom for more info about query |
+| all | `callback`| get all rows of table from the model |
+#### all params with `*`  required
+
+## Query
+* Query setters
+  * `where(value1, value2)`
+  * `orderBy(field)`
+  * `orderByKey(key)`
+  * `limit(limit = 25)`
+* Query getters
+  * `first(callback)`
+  * `last(callback)`
+  * `get(callback)`
+
+```
+Post.where('New title').orderBy('pub_date').limit(10).get((posts) => {
+    this.posts = posts;
+});
+```
+
+# Example
+### Posts.js
+```js
+'use strict'
+const firebaseorm = require('@componet17/firebaseorm');
+
+export default class Posts extends firebaseorm {
+    constructor() {
+        super();
+
+        this.firebase = {
+            apiKey: "",
+            authDomain: "",
+            databaseURL: "",
+            projectId: "",
+            storageBucket: "",
+            messagingSenderId: ""
+        };
+        this.table = 'posts';
+        /** with validation **/
+        this.fields = {
+            name: {
+                required: true,
+                type: Number,
+                min: 3,
+                max: 20
+            },
+            email: {
+                required: true,
+                type: 'email',
+            },
+            password: {
+                required: true,
+                type: String,
+                regex: /^[a-z0-9]{6,32}$/i
+            },
+            phone: {
+                required: false,
+                type: Number,
+                defaults: 89663632121
+            }
+        };
+        /** without validation**/
+        //this.fields = ['name', 'email', 'password', 'phone'];
+
+        /** don't forget call __construct method! **/
+        this.__construct();
+
+    };
+
+    user = () => {
+        console.log('user');
+        return this.belogonsTo('test', 'user');
+    };
+
+    MTM = () => {
+        return this.belongsToMany('post-user', 'post', 'test');
+    };
+
+}
+```
+
+### Vue example
+```
+    import Posts from './Posts';
+    export default {
+        data() {
+            return {
+                posts: {},
+                post: {}
+            }
+        },
+
+
+        methods: {
+             query() {
+                //
+             },
+             manyToMany() {
+                let Post = new Posts;
+                let User = new Users;
+                Post.find('-Kr0OupE6kfS38QDrJff').then((post) => {
+                    User.find('-KqxS6Gm0vtBUJT-ng-3').then((user) => {
+                        Post.MTM().attach(User);
+                        //Post.MTM().attach(User2);
+                        //Post.MTM().attach(User3);
+                    });
+                });
+             },
+
+             getManyToMany() {
+                let Post = new Posts;
+                let User = new Users;
+                Post.find('-Kr0OupE6kfS38QDrJff').then((post) => {
+                    Post.MTM().get((data) => {
+                        console.log(data);
+                    });
+                });
+            }
+
+             belognsTo() {
+                let User = new Users();
+                User.find('-Kqw4Tvobvdr-RbtX8ZO').then((data) => {
+                    let Post = new Posts();
+                    Post.title = 123;
+                    Post.text = `lorem50`;
+                    Post.save().then((post) => {
+                        Post.user().attach(User);
+                    });
+
+                }).catch((e) => {
+                    console.error(e);
+                })
+
+            },
+
+            findPost() {
+                let Post = new Posts();
+                /**
+                    with callback for reactivity
+                **/
+                Post.find('-postid', (post) => {
+                    //reactive update with callback
+                    this.post = post;
+                    // post = Post Model with data
+                    // post.save(), post.delete() and any methods works
+                }).then((post) => {
+                    setTimeout(() => {
+                        Post.title = "New title";
+                        Post.save();
+                    }, 10000); //this.post.title = "New title";
+                });
+
+                /**
+                    just get post data
+                **/
+                Post.find('-badpostid').then((post) => {
+                    this.post = post;
+                }).catch( e =>  console.error(e);//row not found );
+
+            },
+
+
+            editPost() {
+                Post.find('-postid').then((post) => {
+                    Post.title = "New title";
+                    Post.save();
+                }).catch((e) => {
+                    console.error(e);
+                });
+            }
+
+            deletePost() {
+                let Post = new Posts();
+                Post.delete('-postid');
+            },
+
+            createPost() {
+                let Post = new Posts();
+                Post.title = "My first post";
+                Post.text  = "lorem...";
+                Post.save().then((post) => {
+                    console.log(post);
+                    this.post = post;
+                }).catch((e) => {
+                    console.error(e);
+                });
+            }
+        }
+    }
+```
